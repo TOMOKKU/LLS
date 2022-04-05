dee22b449e8b493e2fa2c79995ba96765efcfb7b
TOMOKKU <69345378+TOMOKKU@users.noreply.github.com>
2022-04-05 15:08:51 +0900

    created devise user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,6 +72,17 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttles_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "メールアドレス",
+    :password => "パスワード",
+    :authentication => 'login' 
+  }
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  root to: "home#top"
+  devise_for :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
