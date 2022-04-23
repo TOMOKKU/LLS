534502a470c5e95ba2b9f04d700d1ba693b37a54
TOMOKKU <69345378+TOMOKKU@users.noreply.github.com>
2022-04-23 13:09:02 +0900

    created foods model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_040043) do
+ActiveRecord::Schema.define(version: 2022_04_23_035558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.integer "fleshness"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
