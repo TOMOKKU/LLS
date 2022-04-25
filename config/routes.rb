0df847b9f4b04b4815fda381a6129e7220bebc19
TOMOKKU <69345378+TOMOKKU@users.noreply.github.com>
2022-04-25 23:16:59 +0900

    created fundation of users

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    :confirmations => 'users/confirmations',
+    :registrations => 'users/registrations', 
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords'
+  }
+
   get 'vegetable/index'
   get 'users/index'
   root to: "home#top"
-  devise_for :users, controllers: {
-    registrations: 'users/registrations', 
-    sessions: 'users/sessions'
-  }
 
   devise_scope :user do
     get "sign_in", :to => "users/sessions#new"
     get "sign_out", :to => "users/sessions#destroy"
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
