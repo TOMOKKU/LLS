1e59915e481efcdf745e3b41ad20734e648c6600
TOMOKKU <69345378+TOMOKKU@users.noreply.github.com>
2022-05-09 07:59:48 +0900

    ch app pass

--- test/controllers/vegetable_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class VegetableControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get vegetable_index_url
-    assert_response :success
-  end
-end
